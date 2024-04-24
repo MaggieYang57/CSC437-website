9bd19be62fe8f8d0325b70df1119363cd69eb209
Maggie Yang <myang57@calpoly.edu>
2024-04-24 11:03:31 -0700

    added kebab and light mode

--- packages/proto/public/js/kebab-menu.js
+++ packages/proto/public/js/kebab-menu.js
@@ -1,62 +1,62 @@
+import { prepareTemplate } from "./template.js";
 
 export class KebabMenu extends HTMLElement {
     parser = new DOMParser();
 
+    // <object data="./images/kebab-menu.svg" width="30px"></object> inside slot doesn't work
+    // where do i do :hover styling for shadowDom
     static template = prepareTemplate(`<template>
     <slot name="actuator">
-        <object data="./images/kebab-menu.svg" width="30px"></object>
+        <button>Menu</button
     </slot>
     <div id="panel">
         <slot></slot>
     </div>
-    <style> /* styling */ </style>
+    <style>
+        :host {
+            position: fixed;
+            float: right;
+            cursor: pointer
+        }
+        #panel {
+            display: none;
+            position: left;
+            width: max-content;
+            padding: 5px 15px;
+            border-width: 1px;
+            border-color: var(--color-background-primary);
+            border-style: solid;
+            border-radius: var(--border-radius-small);
+            background: var(--color-card-first);
+        }
+        :host([open]) #panel {
+            display: block;
+        }
+        div:hover{
+                background: lighten(grey, 30%);
+        }
+
+        object{
+            color: var(--color-background-primary)
+        }
+    </style>
     </template>`);
     
     constructor() {
         super();
-        this.attachShadow({ mode: "open" }).appendChild(
-        DropDownElement.template.cloneNode(true)
-        );
-        
-        this.button = this.shadowRoot.querySelector("slot[name='actuator']")
-        this.panel = this.shadowRoot.querySelector("#panel")
 
-        this.button.addEventListener("click", () => this.toggle());
+        this.attachShadow({ mode: "open" }).appendChild(
+        KebabMenu.template.cloneNode(true)
+        );
+        this.shadowRoot
+        .querySelector("slot[name='actuator']")
+        .addEventListener("click", () => this.toggle());
     }
 
     toggle() {
-        if (this.panel.hasAttribute('open')) {
-            this.panel.removeAttribute('open');
-        } else {
-            this.panel.setAttribute('open', '');
-        }
+        if (this.hasAttribute("open")) this.removeAttribute("open");
+        else this.setAttribute("open", "open");
     }
-
-    // loadDetails(href, container) {
-    //     console.log("Loading details from:", href);
-    //     fetch(href)
-    //     .then((response) => {
-    //       if (response.status !== 200) {
-    //         throw `Status: ${response.status}`;
-    //       }
-    //       return response.text();
-    //     })
-    //     .then((htmlString) => this.addFragment(htmlString, container))
-    //     .catch((error) =>
-    //       this.addFragment(
-    //         `<p class="error">
-    //         Failed to fetch ${href}: ${error}
-    //         </p>`,
-    //         container
-    //       )
-    //     );
-    // }
-
-    // addFragment(htmlString, container) {
-    //     const doc = this.parser.parseFromString(htmlString, "text/html");
-    //     const fragment = Array.from(doc.body.childNodes);
-    //     container.append(...fragment);
-    // }
 }
 
 customElements.define("kebab-menu", KebabMenu);
