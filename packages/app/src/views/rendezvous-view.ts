dff6466ff19504292c34ec5259f06a3198d52bdf
Maggie Yang <myang57@calpoly.edu>
2024-06-05 12:19:20 -0700

    fix url

--- packages/app/src/views/rendezvous-view.ts
+++ packages/app/src/views/rendezvous-view.ts
@@ -181,7 +181,7 @@ import { state } from "lit/decorators.js";
               <h2 class="dates">4.12.24 - 4.14.24</h2>
               <h2>COACHELLA</h2>
               <h3>SLOmies</h3>
-              <expand-info href='./public/data/rendezvous/coachella.html'>
+              <expand-info href='./data/rendezvous/coachella.html'>
               </expand-info>
             </div>
           </div>
@@ -190,7 +190,7 @@ import { state } from "lit/decorators.js";
               <h2 class="dates">5.17.24 - 5.19.24</h2>
               <h2>EDC LV</h2>
               <h3>The Best Group</h3>
-              <expand-info href='./public/data/rendezvous/edc.html'>
+              <expand-info href='./data/rendezvous/edc.html'>
               </expand-info>
             </div>
           </div>
